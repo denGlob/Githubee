68e1d1faca231bbd038c93e1029d0be45d50b8f0
Denys Smishny <denshiksmile@gmail.com>
2020-02-12 23:47:32 +0200

    - гзвфеув response models

--- app/src/main/java/com/denshiksmle/githubee/domain/entities/DetailUser.java
+++ app/src/main/java/com/denshiksmle/githubee/domain/entities/DetailUser.java
@@ -1,7 +1,84 @@
 package com.denshiksmle.githubee.domain.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class DetailUser implements Serializable {
+    @SerializedName("id") // We can use @Expose annotation insteadof @SerializedName :))
+    private String id;
+    @SerializedName("login")
+    private String login;
+    @SerializedName("avatar_url")
+    private String avatarUrl;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("url")
+    private String url;
+    @SerializedName("repos_url")
+    private String reposUrl;
+    @SerializedName("public_repos")
+    private int publicReposCount;
+    @SerializedName("gists_url")
+    private String gistsUrl;
+    @SerializedName("public_gists")
+    private int publicGistsCount;
+    @SerializedName("followers")
+    private int followersCount;
 
+    public String getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getReposUrl() {
+        return reposUrl;
+    }
+
+    public int getPublicReposCount() {
+        return publicReposCount;
+    }
+
+    public String getGistsUrl() {
+        return gistsUrl;
+    }
+
+    public int getPublicGistsCount() {
+        return publicGistsCount;
+    }
+
+    public int getFollowersCount() {
+        return followersCount;
+    }
+
+    @Override
+    public String toString() {
+        return "DetailUser{" +
+                "id='" + id + '\'' +
+                ", login='" + login + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                ", name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", reposUrl='" + reposUrl + '\'' +
+                ", publicReposCount=" + publicReposCount +
+                ", gistsUrl='" + gistsUrl + '\'' +
+                ", publicGistsCount=" + publicGistsCount +
+                ", followersCount=" + followersCount +
+                '}';
+    }
 }
--- app/src/main/java/com/denshiksmle/githubee/domain/entities/User.java
+++ app/src/main/java/com/denshiksmle/githubee/domain/entities/User.java
@@ -1,7 +1,35 @@
 package com.denshiksmle.githubee.domain.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
+    @SerializedName("id") // We can use @Expose annotation insteadof @SerializedName :))
+    private String id;
+    @SerializedName("login")
+    private String login;
+    @SerializedName("avatar_url")
+    private String avatarUrl;
 
+    public String getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", login='" + login + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                '}';
+    }
 }
